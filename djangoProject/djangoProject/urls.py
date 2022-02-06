51e6e8dc500b8505ea8464f5dc44528c75460012
disooloo <disooloo@mail.ru>
2022-02-06 17:48:26 +0600

    init

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -5,4 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('blogs', include('blogs.urls')),
+    path('weather', include('weather.urls')),
+
 ]
